155821959d37a81ea16ad7110bd69eb0eea37b03
siddhantCodes <siddhantk232@gmail.com>
2024-05-01 10:01:12 +0530

    impl login()

--- auth/src/handlers/login.rs
+++ auth/src/handlers/login.rs
@@ -1,8 +1,49 @@
 use auth::layout::{Auth, AuthError};
 
 struct Login {
-    username: String,
-    password: String,
+    user_id: ft_sdk::auth::UserId,
+    identity: String,
+}
+
+impl Login {
+    /// Check if the password matches the hashed password in the database
+    fn match_password(ud: &Vec<ft_sdk::auth::UserData>, password: &str) -> bool {
+        for user_data in ud {
+            if let ft_sdk::auth::UserData::Custom { key, value } = user_data {
+                if key == "hashed_password" {
+                    let stored_password = value.as_str().expect("Expected password to be a string");
+
+                    let parsed_hash = argon2::PasswordHash::new(stored_password)
+                        .expect("Expected password to be a valid hash");
+
+                    let password_match = argon2::PasswordVerifier::verify_password(
+                        &argon2::Argon2::default(),
+                        password.as_bytes(),
+                        &parsed_hash,
+                    );
+
+                    if password_match.is_ok() {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        // User probably has no hashed_password. They can set it via reset
+        // password feature if they used some other auth provider
+        // (github oauth for example)
+        false
+    }
+
+    fn get_identity(ud: &Vec<ft_sdk::auth::UserData>) -> Option<String> {
+        for user_data in ud {
+            if let ft_sdk::auth::UserData::Identity(identity) = user_data {
+                return Some(identity.clone());
+            }
+        }
+
+        None
+    }
 }
 
 impl ft_sdk::Action<Auth, AuthError> for Login {
@@ -19,11 +60,11 @@ impl ft_sdk::Action<Auth, AuthError> for Login {
 
         let mut errors = std::collections::HashMap::new();
 
-        let username = get_required_json_field(&body, "username");
+        let email = get_required_json_field(&body, "email");
         let password = get_required_json_field(&body, "password");
 
-        if let Err(_) = username {
-            errors.insert("username".into(), "username/email is required".into());
+        if let Err(_) = email {
+            errors.insert("email".into(), "email is required".into());
         }
 
         if let Err(_) = password {
@@ -34,123 +75,53 @@ impl ft_sdk::Action<Auth, AuthError> for Login {
             return Err(AuthError::FormError(errors));
         }
 
-        let username = username.unwrap();
+        let email = email.unwrap();
         let password = password.unwrap();
 
-        todo!();
+        let (user_id, user_data) =
+            ft_sdk::auth_provider::get_user_data_by_email(&mut c.conn, auth::PROVIDER_ID, &email)
+                .map_err(user_data_error_to_auth_err)?;
 
-        // let query = fastn_core::schema::fastn_user::table
-        //     .filter(fastn_core::schema::fastn_user::username.eq(&payload.username))
-        //     .or_filter(
-        //         fastn_core::schema::fastn_user::email
-        //             .eq(fastn_core::utils::citext(&payload.username)),
-        //     )
-        //     .select(fastn_core::auth::FastnUser::as_select());
-        //
-        // let user: Option<fastn_core::auth::FastnUser> = query.first(&mut conn).await.optional()?;
-        //
-        // if user.is_none() {
-        //     return fastn_core::http::user_err(
-        //         vec![("username".into(), vec!["invalid email/username".into()])],
-        //         fastn_core::http::StatusCode::OK,
-        //     );
-        // }
-        //
-        // let user = user.expect("expected user to be Some");
-        //
-        // // OAuth users don't have password
-        // if user.password.is_empty() {
-        //     // TODO: create feature to ask if the user wants to convert their account to an email
-        //     // password
-        //     // or should we redirect them to the oauth provider they used last time?
-        //     // redirecting them will require saving the method they used to login which de don't atm
-        //     return fastn_core::http::user_err(
-        //         vec![("username".into(), vec!["invalid username".into()])],
-        //         fastn_core::http::StatusCode::OK,
-        //     );
-        // }
-        //
-        // let parsed_hash = argon2::PasswordHash::new(&user.password).map_err(|e| {
-        //     fastn_core::Error::generic(format!("failed to parse hashed password: {e}"))
-        // })?;
-        //
-        // let password_match = argon2::PasswordVerifier::verify_password(
-        //     &argon2::Argon2::default(),
-        //     payload.password.as_bytes(),
-        //     &parsed_hash,
-        // );
-        //
-        // if password_match.is_err() {
-        //     return fastn_core::http::user_err(
-        //         vec![(
-        //             "password".into(),
-        //             vec!["incorrect username/password".into()],
-        //         )],
-        //         fastn_core::http::StatusCode::OK,
-        //     );
-        // }
+        if !Login::match_password(&user_data, &password) {
+            return Err(AuthError::form_error(
+                "password",
+                "incorrect email/password",
+            ));
+        }
+
+        let identity = Login::get_identity(&user_data).expect(
+            "Expected identity to be present in user data. All providers must provide an identity",
+        );
+
+        Ok(Login { user_id, identity })
     }
 
     fn action(&self, c: &mut Auth) -> Result<ft_sdk::ActionOutput, AuthError>
     where
         Self: Sized,
     {
-        todo!()
+        ft_sdk::auth_provider::login(
+            &mut c.conn,
+            &mut c.in_.set_cookies,
+            &self.user_id,
+            "email",
+            &self.identity,
+        )?;
+
+        let mut resp_json = std::collections::HashMap::new();
+
+        resp_json.insert("message".to_string(), "login successfull".into());
+        resp_json.insert("success".to_string(), true.into());
+
+        Ok(ft_sdk::ActionOutput::Data(resp_json))
     }
 }
 
-// pub(crate) async fn login(
-//     req_config: &mut fastn_core::RequestConfig,
-//     db_pool: &fastn_core::db::PgPool,
-//     next: String,
-// ) -> fastn_core::Result<fastn_core::http::Response> {
-//     use diesel::prelude::*;
-//     use diesel_async::RunQueryDsl;
-//
-//     if req_config.request.method() != "POST" {
-//         // TODO: if user is logged in redirect to next
-//
-//         let main = fastn_core::Document {
-//             package_name: req_config.config.package.name.clone(),
-//             id: fastn_core::auth::Route::Login.to_string(),
-//             content: fastn_core::auth::email_password::login_ftd().to_string(),
-//             parent_path: fastn_ds::Path::new("/"),
-//         };
-//
-//         let resp = fastn_core::package::package_doc::read_ftd(req_config, &main, "/", false, false)
-//             .await?;
-//
-//         return Ok(resp.into());
-//     }
-//
-//     #[derive(serde::Deserialize, validator::Validate, Debug)]
-//     struct Payload {
-//         username: String,
-//         password: String,
-//     }
-//
-//     let now = chrono::Utc::now();
-//
-//     // TODO: session should store device that was used to login (chrome desktop on windows)
-//     let session_id: i64 = diesel::insert_into(fastn_core::schema::fastn_auth_session::table)
-//         .values((
-//             fastn_core::schema::fastn_auth_session::user_id.eq(&user.id),
-//             fastn_core::schema::fastn_auth_session::created_at.eq(now),
-//             fastn_core::schema::fastn_auth_session::updated_at.eq(now),
-//         ))
-//         .returning(fastn_core::schema::fastn_auth_session::id)
-//         .get_result(&mut conn)
-//         .await?;
-//
-//     tracing::info!("session created. session id: {}", &session_id);
-//
-//     // client has to 'follow' this request
-//     // https://stackoverflow.com/a/39739894
-//     fastn_core::auth::set_session_cookie_and_redirect_to_next(
-//         &req_config.request,
-//         &req_config.config.ds,
-//         session_id,
-//         next,
-//     )
-//     .await
-// }
+fn user_data_error_to_auth_err(e: ft_sdk::auth_provider::UserDataError) -> AuthError {
+    match e {
+        ft_sdk::auth_provider::UserDataError::NoDataFound => {
+            AuthError::form_error("email", "invalid email")
+        }
+        ft_sdk::auth_provider::UserDataError::DatabaseError(d) => AuthError::Diesel(d),
+    }
+}
