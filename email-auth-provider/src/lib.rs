9cdbaec218802e7e648d2510327c3506286bcb0e
siddhantCodes <siddhantk232@gmail.com>
2024-08-26 18:06:38 +0530

    Impl "reset password request"
    A forgot_password form is added that send a password reset link to
    user's email.

--- email-auth-provider/src/lib.rs
+++ email-auth-provider/src/lib.rs
@@ -3,7 +3,11 @@ extern crate self as email_auth;
 mod handlers;
 mod urls;
 
+pub(crate) use handlers::utils;
+
 pub const PROVIDER_ID: &str = "email";
 pub const SUBSCRIPTION_PROVIDER_ID: &str = "subscription";
 pub const EMAIL_CONF_CODE_KEY: &str = "email_confirmation_code";
+pub const PASSWORD_RESET_CODE_KEY: &str = "password_reset_code";
+pub const PASSWORD_RESET_CODE_SENT_AT: &str = "password_reset_code_sent_at";
 pub const EMAIL_CONF_SENT_AT: &str = "email_conf_sent_at";
