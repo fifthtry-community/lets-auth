eba0c9986709699695c340b4602c1ba60070461e
Amit Upadhyay <upadhyay@gmail.com>
2025-01-25 12:14:13 +0530

    better alert.js

--- app/.packages/lets-auth-system.fifthtry.site/actions/alert.js
+++ app/.packages/lets-auth-system.fifthtry.site/actions/alert.js
@@ -1,3 +1,21 @@
 function show_alert() {
-    alert(JSON.stringify(arguments))
+    let message = "";
+    for (let i = 0; i < arguments.length; i++) {
+        let argument = arguments[i]
+        if (argument instanceof fastn.recordInstanceClass) {
+            let error = argument.get("error").get();
+            if (error) {
+                error = " [error: " + error + "]";
+            } else {
+                error = " [no error]";
+            }
+            message += (
+                argument.get("name").get() + ": " + argument.get("value").get() + error + "\n"
+            );
+        } else {
+            message += argument + "\n";
+        }
+    }
+
+    alert(message);
 }
