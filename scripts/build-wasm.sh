9247874336a457af756838954c7b5866f510051d
Amit Upadhyay <upadhyay@gmail.com>
2025-01-29 13:33:20 +0530

    its lets-auth not lets-talk

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -11,7 +11,7 @@ SOURCE1="./target/wasm32-unknown-unknown/release/"
 SOURCE2="$HOME/target/wasm32-unknown-unknown/release/"
 
 # Define destination folders
-DEST="./lets-talk.fifthtry.site"
+DEST="./lets-auth.fifthtry.site"
 
 # Ensure WASM files exist and determine the source folder to use
 if [ -d "$SOURCE1" ]; then
@@ -22,9 +22,6 @@ else
     echo "Source folder not found."
     exit 1
 fi
-
-# Ensure the destination folders exist
-mkdir -p $DEST
 
 # Copy files to destinations
 cp "${SOURCE_DIR}mobile_auth_provider.wasm" "$DEST"
--- scripts/publish-app.sh
+++ scripts/publish-app.sh
@@ -3,8 +3,6 @@
 set -uxe
 set -o pipefail
 
-sh -c "$(curl -fsSL https://fastn.com/install.sh)"
-
 ./scripts/build-wasm.sh || exit 1
 ./scripts/optimise-wasm.sh || exit 1
 
@@ -13,5 +11,7 @@ rm .gitignore
 echo .packages > .gitignore
 echo .fastn >> .gitignore
 echo .is-local >> .gitignore
+
+sh -c "$(curl -fsSL https://fastn.com/install.sh)"
 
 cd lets-auth.fifthtry.site/ && fastn upload lets-auth
