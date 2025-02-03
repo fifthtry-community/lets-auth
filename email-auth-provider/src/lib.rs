a0b6e4c9e08b98e8fd7c903bdf84efceb926ec84
Amit Upadhyay <upadhyay@gmail.com>
2025-02-03 18:37:19 +0530

    using ft_sdk::Config extractor

--- email-auth-provider/src/lib.rs
+++ email-auth-provider/src/lib.rs
@@ -3,10 +3,7 @@ extern crate self as email_auth;
 mod handlers;
 mod urls;
 
-use ft_sdk::Error;
 pub(crate) use handlers::utils;
-use http::Request;
-use serde_json::Value;
 
 pub const PROVIDER_ID: &str = "email";
 pub const SUBSCRIPTION_PROVIDER_ID: &str = "subscription";
@@ -21,23 +18,4 @@ pub struct Config {
     pub signin_url: String,
     #[serde(rename = "sign-up-url")]
     pub signup_url: String,
-}
-
-impl ft_sdk::FromRequest for Config {
-    fn from_request(req: &Request<Value>) -> Result<Self, Error> {
-        let scheme = ft_sdk::Scheme::from_request(req)?;
-        let host = ft_sdk::Host::from_request(req)?;
-        let app_url: ft_sdk::AppUrl = ft_sdk::AppUrl::from_request(req)?;
-
-        let url = app_url.join(&scheme, &host, "config")?;
-        ft_sdk::println!("url: {url}");
-
-        let req = http::Request::builder()
-            .uri(url)
-            .body(bytes::Bytes::new())?;
-
-        let res = ft_sdk::http::send(req).unwrap();
-
-        serde_json::from_slice(res.body()).map_err(|e| e.into())
-    }
 }
