c743e9f2d75103083d4443e4de13520fead1be12
siddhantCodes <siddhantk232@gmail.com>
2025-02-07 13:54:04 +0530

    Secure cookies work just fine on localhost, use it everywhere

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -21,13 +21,12 @@ pub fn validate_identity(
 
 pub fn session_cookie(sid: &str, host: ft_sdk::Host) -> Result<http::HeaderValue, ft_sdk::Error> {
     // DO NOT CHANGE THINGS HERE, consult logout code in fastn.
-    let secure_cookies = option_env!("DEV_USE_HTTP_COOKIES").is_none();
     let cookie = cookie::Cookie::build((ft_sdk::auth::SESSION_KEY, sid))
         .domain(host.without_port())
         .path("/")
         .max_age(cookie::time::Duration::seconds(34560000))
         // Secure is required in cross-origin requests otherwise browsers will ignore it
-        .secure(secure_cookies) // Secure cookies only on production
+        .secure(true)
         .build();
 
     Ok(http::HeaderValue::from_str(cookie.to_string().as_str())?)
