cc1af1e375afc0a64cdefec2ffcbaebbc85688fc
siddhantCodes <siddhantk232@gmail.com>
2025-02-21 13:28:32 +0530

    Feat: /set-password/ app url
    This page can be used to change password. This supports both, the
    forgot-password flow where user is coming from a link sent to their
    email and, when the user is already logged in and is simply visiting
    /set-password/ to update their password.

--- email-auth-provider/src/handlers/set_password.rs
+++ email-auth-provider/src/handlers/set_password.rs
@@ -4,59 +4,39 @@ pub fn set_password(
     mut conn: ft_sdk::Connection,
     ft_sdk::Required(new_password): ft_sdk::Required<"new-password">,
     ft_sdk::Required(new_password2): ft_sdk::Required<"new-password2">,
-    ft_sdk::Query(code): ft_sdk::Query<"code">,
-    ft_sdk::Query(spr): ft_sdk::Query<"spr">,
-    ft_sdk::Query(email): ft_sdk::Query<"email">,
+    ft_sdk::Query(code): ft_sdk::Query<"code", Option<String>>,
+    ft_sdk::Query(email): ft_sdk::Query<"email", Option<String>>,
     ft_sdk::Query(next): ft_sdk::Query<"next", Option<String>>,
     host: ft_sdk::Host,
     app_url: ft_sdk::AppUrl,
+    scheme: crate::HTTPSScheme,
+    sid: ft_sdk::Cookie<{ ft_sdk::auth::SESSION_KEY }>,
     ft_sdk::Config(config): ft_sdk::Config<crate::Config>,
 ) -> ft_sdk::form::Result {
     validate_email_and_password(&email, &new_password, &new_password2)?;
 
     let next = next.unwrap_or_else(|| "/".to_string());
 
-    let (user_id, data) = match ft_sdk::auth::provider::user_data_by_custom_attribute(
-        &mut conn,
-        email_auth::PROVIDER_ID,
-        email_auth::PASSWORD_RESET_CODE_KEY,
-        &code,
-    ) {
-        Ok(value) => value,
-        Err(ft_sdk::auth::UserDataError::NoDataFound) => return ft_sdk::form::redirect(next),
-        Err(e) => return Err(e.into()),
-    };
+    let (user_id, data) = get_user(&mut conn, sid, code)?;
 
-    let sent_at = data
-        .get_custom(email_auth::PASSWORD_RESET_CODE_SENT_AT)
-        .expect("PASSWORD_RESET_CODE_SENT_AT should exists if the account was found");
-    let sent_at = chrono::DateTime::from_timestamp_nanos(sent_at);
+    let sent_at = data.get_custom(email_auth::PASSWORD_RESET_CODE_SENT_AT);
 
-    if key_expired(sent_at) {
-        let reset_link = email_auth::handlers::forgot_password::generate_new_reset_key(
-            data.clone(),
-            &user_id,
-            &email,
-            spr,
-            &host,
-            &mut conn,
-            app_url,
-        )?;
+    if let Some(sent_at) = sent_at {
+        let set_password_url = app_url
+            .join(&scheme, &host, "/set-password/")
+            .inspect_err(|e| {
+                ft_sdk::println!("auth.wasm: failed to join url: {:?}", e);
+            })?;
 
-        let name = data.name.unwrap_or_else(|| email.clone());
-
-        email_auth::handlers::forgot_password::send_reset_password_email(
+        check_expired_and_send_reset_link(
+            set_password_url,
+            sent_at,
+            &data,
+            user_id.clone(),
             email,
-            name,
-            &reset_link,
-            &config,
+            &mut conn,
+            config,
         )?;
-
-        return Err(ft_sdk::single_error(
-            "code",
-            "Confirmation code expired. A new link has been sent to your email address.",
-        )
-        .into());
     }
 
     let data = {
@@ -101,11 +81,11 @@ fn key_expired(sent_at: chrono::DateTime<chrono::Utc>) -> bool {
 }
 
 fn validate_email_and_password(
-    email: &str,
+    email: &Option<String>,
     new_password: &str,
     new_password2: &str,
 ) -> Result<(), ft_sdk::Error> {
-    if !validator::ValidateEmail::validate_email(&email) {
+    if email.is_some() && !validator::ValidateEmail::validate_email(email.as_ref().unwrap()) {
         return Err(ft_sdk::single_error("email", "Invalid email format.").into());
     }
 
@@ -122,4 +102,85 @@ fn validate_email_and_password(
     }
 
     Ok(())
+}
+
+/// Get logged in user or user with the reset code
+fn get_user(
+    conn: &mut ft_sdk::Connection,
+    sid: ft_sdk::Cookie<{ ft_sdk::auth::SESSION_KEY }>,
+    reset_code: Option<String>,
+) -> Result<(ft_sdk::UserId, ft_sdk::auth::ProviderData), ft_sdk::Error> {
+    let user = ft_sdk::auth::ud(sid, conn).ok().flatten().map(|v| v.id);
+
+    let res = if let Some(user_id) = user {
+        // if user is logged in, we can use the user_id to get the user data
+        ft_sdk::auth::provider::user_data_by_id(
+            conn,
+            email_auth::PROVIDER_ID,
+            &ft_sdk::UserId(user_id),
+        )
+        .map(|v| (ft_sdk::UserId(user_id), v))
+    } else {
+        // if user is not logged in, we can use the code to get the user data
+        let reset_code = reset_code
+            .ok_or_else(|| ft_sdk::single_error("code", "Invalid reset code and not logged in."))?;
+
+        ft_sdk::auth::provider::user_data_by_custom_attribute(
+            conn,
+            email_auth::PROVIDER_ID,
+            email_auth::PASSWORD_RESET_CODE_KEY,
+            &reset_code,
+        )
+    };
+
+    match res {
+        Ok(v) => Ok(v),
+        Err(ft_sdk::auth::UserDataError::NoDataFound) => {
+            Err(ft_sdk::single_error("code", "Invalid reset code or not logged in.").into())
+        }
+        Err(e) => Err(e.into()),
+    }
+}
+
+fn check_expired_and_send_reset_link(
+    set_password_url: String,
+    sent_at: i64,
+    data: &ft_sdk::auth::ProviderData,
+    user_id: ft_sdk::UserId,
+    email: Option<String>,
+    conn: &mut ft_sdk::Connection,
+    config: crate::Config,
+) -> Result<(), ft_sdk::Error> {
+    let sent_at = chrono::DateTime::from_timestamp_nanos(sent_at);
+
+    if !key_expired(sent_at) {
+        return Ok(());
+    }
+
+    let email = email.ok_or_else(|| {
+        ft_sdk::single_error("email", "Email is required if you're not logged in.")
+    })?;
+
+    let reset_link = email_auth::handlers::forgot_password::generate_new_reset_key(
+        data.clone(),
+        &user_id,
+        &email,
+        set_password_url,
+        conn,
+    )?;
+
+    let name = data.name.clone().unwrap_or_else(|| email.to_string());
+
+    email_auth::handlers::forgot_password::send_reset_password_email(
+        email.to_string(),
+        name,
+        &reset_link,
+        &config,
+    )?;
+
+    Err(ft_sdk::single_error(
+        "code",
+        "Confirmation code expired. A new link has been sent to your email address.",
+    )
+    .into())
 }
