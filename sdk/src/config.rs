657da2662cce980b684be4737ce2d5231d5bc408
siddhantCodes <siddhantk232@gmail.com>
2025-03-22 14:59:51 +0530

    Use ft-sdk 0.6.2 to fix CI
    0.6.3 of ft-sdk is not released yet. The code was using some of the
    features and it was compiling because of the local copy of ft-sdk. This
    however does not work in CI.

--- sdk/src/config.rs
+++ sdk/src/config.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+// #[derive(Debug)] // TODO: uncomment when ft-sdk 0.6.3 is out
 pub struct Config {
     pub app_url: lets_auth::AppUrl,
     pub email_sender_name: String,
@@ -8,25 +8,27 @@ pub struct Config {
 }
 
 impl ft_sdk::FromRequest for Config {
-    fn from_request(req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error> {
-        #[derive(Debug, serde::Deserialize)]
-        #[serde(rename_all = "kebab-case")]
-        pub struct C {
-            email_sender_name: String,
-            email_reply_to: String,
-            super_user_id: i64,
-            is_personal_site: bool,
-        }
-
-        let c: C =
-            ft_sdk::Config::from_request_for_key(lets_auth::SYSTEM, req).map(|c| c.into())?;
-
-        Ok(Config {
-            app_url: ft_sdk::FromRequest::from_request(req)?,
-            email_sender_name: c.email_sender_name,
-            email_reply_to: c.email_reply_to,
-            super_user_id: c.super_user_id,
-            is_personal_site: c.is_personal_site,
-        })
+    fn from_request(_req: &http::Request<serde_json::Value>) -> Result<Self, ft_sdk::Error> {
+        todo!()
+        // TODO: Uncomment when ft-sdk 0.6.3 is out
+        // #[derive(Debug, serde::Deserialize)]
+        // #[serde(rename_all = "kebab-case")]
+        // pub struct C {
+        //     email_sender_name: String,
+        //     email_reply_to: String,
+        //     super_user_id: i64,
+        //     is_personal_site: bool,
+        // }
+        //
+        // let c: C =
+        //     ft_sdk::Config::from_request_for_key(lets_auth::SYSTEM, req).map(|c| c.into())?;
+        //
+        // Ok(Config {
+        //     app_url: ft_sdk::FromRequest::from_request(req)?,
+        //     email_sender_name: c.email_sender_name,
+        //     email_reply_to: c.email_reply_to,
+        //     super_user_id: c.super_user_id,
+        //     is_personal_site: c.is_personal_site,
+        // })
     }
 }
